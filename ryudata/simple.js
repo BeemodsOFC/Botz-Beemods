80d10e29570462048f19a8d3b5eca777d541a85e
BeemodsOFC <98037497+BeemodsOFC@users.noreply.github.com>
2022-02-08 14:42:25 +0700

    Add files via upload

---
+++ ryudata/simple.js
@@ -0,0 +1,254 @@
+//SILAHKAN EDIT SESUKA KALIAN
+
+//MENU AWAL
+exports.smenu = (pushname, namabot) => {
+	return`Halo Kak ${pushname} 👋
+Saya ${namabot} Siap Melayani Anda, Bot Ini Memakai Program
+Multi Device Beta Dari WhatsApp Messenger.
+
+Jadi Jika Menemukan Bug Silahkan Laporkan Kepada Owner
+Atau Creator Agar Segera Di Fix 🤗
+
+Terima Kasih 🙏
+`
+}
+
+//LIST MENU
+exports.list = (pushname, ucapanWaktu, namabot) => {
+	return`*Halo Kak ${pushname} 👋*
+*${ucapanWaktu}*
+
+*Berikut List Menu Yang Tersedia Di ${namabot}*
+
+*Jika Menemukan Bug Harap Dimaklumi Dan Laporkan Kepada*
+*Owner Atau Creator Agar Segera Di Fix 😎*
+
+*Terima Kasih 🙏*
+`
+}
+
+//MENU OWNER
+exports.sowner = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*👤 OWNER MENU*
+⚘🍁 ${prefix}chat [option]
+⚘🍁 ${prefix}join [link]
+⚘🍁 ${prefix}leave
+⚘🍁 ${prefix}self
+⚘🍁 ${prefix}public
+⚘🍁 ${prefix}setppbot
+⚘🍁 ${prefix}block @user
+⚘🍁 ${prefix}unblock @user
+⚘🍁 ${prefix}bcgc
+`
+}
+
+//MENU GRUP
+exports.sgrup = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*👥 GROUP MENU*
+⚘🍁 ${prefix}antilink
+⚘🍁 ${prefix}linkgroup
+⚘🍁 ${prefix}ephemeral [option]
+⚘🍁 ${prefix}setpp
+⚘🍁 ${prefix}setname [text]
+⚘🍁 ${prefix}group [option]
+⚘🍁 ${prefix}add @user
+⚘🍁 ${prefix}kick @user
+⚘🍁 ${prefix}promote @user
+⚘🍁 ${prefix}demote @user
+`
+}
+
+//MENU DOWNLOAD
+exports.sdown = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*📦 DOWNLOAD MENU*
+⚘🍁 ${prefix}tiktok [url]
+⚘🍁 ${prefix}instagram [url]
+⚘🍁 ${prefix}twitter [url]
+⚘🍁 ${prefix}facebook [url]
+⚘🍁 ${prefix}ytmp4 [url]
+⚘🍁 ${prefix}ytmp3 [url]
+⚘🍁 ${prefix}play [judul]
+⚘🍁 ${prefix}ytsearch [judul]
+`
+}
+
+//MENU SEARCH
+exports.ssearch = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*🔎 SEARCH MENU*
+⚘🍁 ${prefix}pinterest [query]
+⚘🍁 ${prefix}infogempa
+⚘🍁 ${prefix}faktaunik
+⚘🍁 ${prefix}pantun
+`
+}
+
+//MENU TEXTPRO
+exports.stextpro = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*👨‍🎨 TEXTPROME MENU*
+⚘🍁 ${prefix}blackpink [teks]
+⚘🍁 ${prefix}neon [teks]
+⚘🍁 ${prefix}lava [teks]
+⚘🍁 ${prefix}3dbox [teks]
+⚘🍁 ${prefix}haloween [teks]
+⚘🍁 ${prefix}horor [teks]
+`
+}
+
+//MENU ASUPAN
+exports.sasupan = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*👭 ASUPAN MENU*
+⚘🍁 ${prefix}asupanchika
+⚘🍁 ${prefix}asupandelvira
+⚘🍁 ${prefix}asupanbocil
+⚘🍁 ${prefix}asupanukhty
+⚘🍁 ${prefix}asupanghea
+⚘🍁 ${prefix}asupanrika
+⚘🍁 ${prefix}asupangina
+`
+}
+
+//MENU CECAN
+exports.scecan = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*🙆‍♀️ GACHA CECAN*
+⚘🍁 ${prefix}cecanvietnam
+⚘🍁 ${prefix}cecanindo
+⚘🍁 ${prefix}cecanmalay
+⚘🍁 ${prefix}cecankorea
+⚘🍁 ${prefix}cecanjapan
+⚘🍁 ${prefix}cecanthai
+⚘🍁 ${prefix}cecanchina
+`
+}
+
+//CASE FUN MENU
+exports.sfun = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*🎮 FUN MENU*
+⚘🍁 ${prefix}truth
+⚘🍁 ${prefix}dare
+⚘🍁 ${prefix}bagaimanakah
+⚘🍁 ${prefix}kapankah
+⚘🍁 ${prefix}dimanakah
+⚘🍁 ${prefix}suit [✌️/✊/✋]
+⚘🍁 ${prefix}aduayam [🐥/🐤🐓/🐣/🦃]
+`
+}
+
+//MENU ANIME
+exports.sanime = (prefix) => {
+	return`Media Sosial 🌱
+IG : albefremakerhell._
+
+*⛩️ ANIME MENU*
+⚘🍁 ${prefix}quotesanime
+⚘🍁 ${prefix}wallpaper
+⚘🍁 ${prefix}anime
+⚘🍁 ${prefix}waifu
+⚘🍁 ${prefix}loli
+⚘🍁 ${prefix}cry
+⚘🍁 ${prefix}kill
+⚘🍁 ${prefix}hug
+⚘🍁 ${prefix}pat
+⚘🍁 ${prefix}lick
+⚘🍁 ${prefix}kiss
+⚘🍁 ${prefix}bite
+⚘🍁 ${prefix}yeet
+⚘🍁 ${prefix}neko
+⚘🍁 ${prefix}bully
+⚘🍁 ${prefix}bonk
+⚘🍁 ${prefix}wink
+⚘🍁 ${prefix}poke
+⚘🍁 ${prefix}nom
+⚘🍁 ${prefix}slap
+⚘🍁 ${prefix}smile
+⚘🍁 ${prefix}wave
+⚘🍁 ${prefix}awoo
+⚘🍁 ${prefix}blush
+⚘🍁 ${prefix}smug
+⚘🍁 ${prefix}glomp
+⚘🍁 ${prefix}happy
+⚘🍁 ${prefix}dance
+⚘🍁 ${prefix}cringe
+⚘🍁 ${prefix}cuddle
+⚘🍁 ${prefix}highfive
+⚘🍁 ${prefix}shinobu
+⚘🍁 ${prefix}megumin
+⚘🍁 ${prefix}handhold
+`
+}
+
+//NSFW MENU
+exports.snsfw = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*🔞 NSFW MENU*
+⚘🍁 ${prefix}nekopoi1
+⚘🍁 ${prefix}nekopoi2
+⚘🍁 ${prefix}nekopoi3
+⚘🍁 ${prefix}nekopoi4
+⚘🍁 ${prefix}nekopoi5
+⚘🍁 ${prefix}nekopoi6
+⚘🍁 ${prefix}nekopoi7
+⚘🍁 ${prefix}nekopoi8
+⚘🍁 ${prefix}nekopoi9
+`
+}
+
+//MAKER MENU
+exports.smaker = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*👨‍🎨 MAKER MENU*
+⚘🍁 ${prefix}toimage
+⚘🍁 ${prefix}smeme
+⚘🍁 ${prefix}smeme2
+⚘🍁 ${prefix}sticker
+⚘🍁 ${prefix}tovideo
+⚘🍁 ${prefix}togif
+⚘🍁 ${prefix}tourl
+`
+}
+
+//OTHER MENU
+exports.sother = (prefix) => {
+	return`Media Sosial 🍁
+Instagram : @albefremakerhell._
+
+*☕ OTHER MENU*
+⚘🍁 ${prefix}ping
+⚘🍁 ${prefix}owner
+⚘🍁 ${prefix}creator
+⚘🍁 ${prefix}menu / ${prefix}help / ${prefix}?
+⚘🍁 ${prefix}delete
+⚘🍁 ${prefix}sc
+⚘🍁 ${prefix}tqto
+⚘🍁 ${prefix}donasi
+⚘🍁 ${prefix}rules
+⚘🍁 ${prefix}sewabot
+`
+}
